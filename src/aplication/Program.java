52eb8c6495a5601106050e9b7b1250c06a058507
Geoo_Loos <geovanna.coura.lopes.2005@gmail.com>
2026-04-08 23:46:40 -0300

    Adicionando seller e mudando program

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,11 +1,18 @@
 package aplication;
 
+import java.util.Date;
+
 import modelEntities.Department;
+import modelEntities.Seller;
 
 public class Program {
     public static void main(String[] args) {
         
         Department obj = new Department(1, "Books");
+
+        Seller obj1 = new Seller(1, "Bob Brown", "bob@gmail.com", new Date(), 1000.0, obj);
         System.out.println(obj);
+        System.out.println(obj1);
+
     }
 }
