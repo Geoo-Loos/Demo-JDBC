4b95bac8b3eeaf45b89b91d880d720d51623a37e
Geoo_Loos <geovanna.coura.lopes.2005@gmail.com>
2026-04-09 21:17:23 -0300

    reuzando a instancia

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -12,7 +12,7 @@ public class Program {
        
 
         SellerDao sellerDao = modelDao.DaoFactory.createSellerDao();
-
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller= sellerDao.findById(3);
         System.out.println(seller);
        
